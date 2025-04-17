81b2e67154b63aea8303c55b46f72a37cae3fa8f
huy.tranduc <duchuyka45@gmail.com>
2025-04-18 00:34:39 +0700

    Add Todo model and update __init__.py to include Todo import

---
+++ common/models/todo.py
@@ -0,0 +1,16 @@
+from rococo.models import VersionedModel
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional
+
+@dataclass
+class Todo(VersionedModel):
+    __tablename__ = 'todo'
+
+    person_id: str = field(default='')
+    title: str = field(default='')
+    priority: int = field(default=0) # 0 = low, 1 = medium, 2 = high
+    is_completed: bool = field(default=False)
+    description: Optional[str] = field(default=None)
+    completed_at: Optional[datetime] = field(default=None)
+    due_date: Optional[datetime] = field(default=None)
